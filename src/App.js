94dc62e5b890c3bd0d220ae969db8aed52900a50
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2020-12-05 16:43:00 -0300

    clean the react files and prepare to development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './penguins.gif';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import { GlobalStyle } from './GlobalStyles.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>oi</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Routes from './routes';
+import App from './App';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
