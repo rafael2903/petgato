f4a6f05565f217d0c4c4ef3f6a76884118e181fa
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2020-12-06 17:27:40 -0300

    fix: wrong background image

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,11 @@
 import StyledLink from '../../components/StyledLink';
 import Input from '../../components/Input';
 import PinkLogo from '../../components/PinkLogo';
-import BackgroundImage from '../../assets/Cadastro.jpg';
+import BackgroundImage from '../../assets/Login.jpg';
 import Divider from '../../components/Divider';
 
 function Login() {
-  
+
   return (
     <Divider src={BackgroundImage}>
       <div>
